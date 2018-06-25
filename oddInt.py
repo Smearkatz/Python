35589b4deb1a358d01f46ce9a16ba763e1a12196
Travis Willoughby <wyverlord@hotmail.com>
2018-06-25 09:49:40 -0500

    Odd Number

---
+++ oddInt.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/find-the-odd-int/train/python
+def find_it(seq):
+    for i in seq:
+        if seq.count(i) % 2 == 1:
+            return i
+
+
+print(find_it([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]))
